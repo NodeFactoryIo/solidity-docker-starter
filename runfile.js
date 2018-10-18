115b73ff8d1f5b4a7bddf90bce1438030ac3d2e4
Marin Petrunić <marin.petrunic@gmail.com>
2018-10-18 13:08:08 +0200

    converted runfile so it doesn't require any dependency

--- runfile.js
+++ runfile.js
@@ -1,29 +1,31 @@
 // tslint:disable-next-line
 const {run, help} = require("runjs");
 
-export function clean() {
+
+
+exports.clean = function (){
   run("docker-compose run --rm smart-contracts rm -rf build");
   run("rm -rf node_modules");
 }
 
-export function lint() {
+exports.lint = function() {
   run("docker-compose run --rm smart-contracts yarn lint");
 }
 
-export function build() {
+exports.build = function() {
   run("docker-compose down");
   run("docker-compose -f docker-compose-cleanup.yml down -v");
   run("docker-compose build");
 }
 
-export function deploy(env: string) {
+exports.deploy = function(env) {
   if (!env) {
     env = "development";
   }
   run(`docker-compose run --rm smart-contracts node_modules/.bin/truffle migrate --network ${env}`);
 }
 
-export function redeploy(env: string) {
+exports.redeploy = function(env) {
   if (!env) {
     env = "development";
   }
@@ -32,67 +34,66 @@ export function redeploy(env: string) {
   );
 }
 
-export function compile() {
+exports.compile = function() {
   run(
     'docker-compose run --no-deps --rm smart-contracts sh -c "yarn generate"',
   );
 }
 
-export function solhint() {
+exports.solhint = function() {
   run(
     "docker-compose run --rm smart-contracts sh -c " +
     '"./node_modules/solhint/solhint.js \\"contracts/**/*.sol\\""',
   );
 }
 
-export function test(testName: string) {
+exports.test = function(testName) {
   if (!testName) {
     testName = "test/*";
   }
-  solhint();
   run(
-    `docker-compose run --rm smart-contracts node_modules/.bin/truffle test ${testName}`,
+    `docker-compose run --rm smart-contracts sh -c "yarn generate && node_modules/.bin/truffle test ${testName}"`,
   );
 }
 
 help(
-  deploy,
+  exports.deploy,
   "Run all missing migrations to deploy contracts" +
   " to ethereum network. Command accepts param with id of network",
 );
 
 help(
-  redeploy,
+  exports.redeploy,
   "Run all migrations again to deploy contracts " +
   "to ethereum network. Command accepts param with id of network",
 );
 
 help(
-  compile,
+  exports.compile,
   "Compiles all contracts and generates typings",
 );
 
 help(
-  solhint,
+  exports.solhint,
   "Runs solhint on all smart contracts",
 );
 
 help(
-  test,
+  exports.test,
   "Runs tests against contracts in docker. " +
   "It accepts test name as optional argument",
 );
 
 help(
-    clean,
+  exports.clean,
     "Removes all build directories and dependencies"
 );
 
 help(
-    lint,
+  exports.lint,
     "Runs tslint on current project"
 );
 help(
-    build,
+  exports.build,
     "Builds new docker image"
 );
