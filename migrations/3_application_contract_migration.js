a889158d5d5332f93d21b0e5d60cfde8a94c903c
Belma Gutlic <belma.dzi@gmail.com>
2019-06-26 13:56:51 +0200

    Use javascript for migrations

--- migrations/3_application_contract_migration.js
+++ migrations/3_application_contract_migration.js
@@ -1,11 +1,6 @@
-import "@types/node";
-import "truffle-typings";
-
 const Application = artifacts.require("./Application.sol");
 const Storage = artifacts.require("./Storage.sol");
 
-module.exports = function(deployer: Truffle.Deployer) {
+module.exports = function(deployer) {
   deployer.deploy(Application, Storage.address);
-} as Truffle.Migration;
-
-export {};
+};
